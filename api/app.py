e3bbb1569a7663af3c8be944cdba413e5a3d29f2
unknown <neelpatel2412@gmail.com>
2025-05-04 21:25:53 +0530

    added css

--- api/app.py
+++ api/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, jsonify, make_response
 from bot2 import chat
 
+
+app = Flask(__name__, static_url_path='/static', static_folder='../static', template_folder='../templates')
+
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
